c412a529701f05cb6f6526763f3366409964ddb0
styrexthereal <styrex.socialmedia@gmail.com>
2023-02-04 20:36:00 +0100

    added constructor to Question class

--- QuizApp/Question.cs
+++ QuizApp/Question.cs
@@ -11,5 +11,12 @@ namespace QuizApp
         public string Text { get; set; }
         public List<string> Answers { get; set; }
         public int CorrectAnswerIndex { get; set; }
+
+        public Question(string text, List<string> answers, int correctAnswerIndex)
+        {
+            Text = text;
+            Answers = answers;
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
     }
 }
